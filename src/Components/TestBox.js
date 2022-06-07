a958a2c48487885ad8bea62266255ba7b317f44e
EdenSweden <19919725+EdenSweden@users.noreply.github.com>
2022-06-07 15:58:37 -0500

    some updates

--- src/Components/TestBox.js
+++ src/Components/TestBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 const boxStyles = {
     'backgroundColor': 'white', 
@@ -7,11 +8,36 @@ const boxStyles = {
     'borderRadius': '15px'
 }
 
+const getData = () => {
+    //only gets max daily temp for now?
+    //Minneapolis weather
+    axios.get('https://api.open-meteo.com/v1/forecast?latitude=44.98&longitude=-93.25&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago')
+    .then(res => {
+        let todaysMax = res.data.daily.temperature_2m_max[0];
+        console.log(todaysMax);
+        return todaysMax;
+        
+    })
+    .catch(err => console.error(err))
+    //always executes:
+    /*.then(data => console.log('data: ' + data));*/
+}
+
+const displayMax = async () => {
+    let result = await getData();
+    console.log('typeof result: ' + typeof result);
+    ;
+}
+
+console.log('displayMax():')
+console.log(displayMax());
+
 export default function TestBox() {
   return (
 <>
 <div style={boxStyles}>
     <p style={{'color': 'black'}}>Hello World</p>
+    {/*<p style={{'color': 'black'}}>{result}</p>*/}
 </div>
 </>
   )
